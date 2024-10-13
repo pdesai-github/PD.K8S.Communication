03f4ccbab341dd9ed7601f68a304b705daba118c
Pradipkumar Desai <pdesai.programming@gmail.com>
2024-10-13 23:07:09 +0530

    Add project files.

---
+++ PD.K8S.API.A/Controllers/ApiAController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace PD.K8S.API.A.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ApiAController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public ApiAController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        // GET: api/<ApiAController>
+        [HttpGet]
+        public async Task<string> Get()
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                var response = await client.GetAsync(_configuration["apib-service-url"]);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var content = await response.Content.ReadAsStringAsync();
+                    return string.Concat("Api A: Hello World!", content);
+                }
+                else
+                {
+                    return "Error";
+                }
+            }     
+
+           
+        }
+      
+    }
+}
---
+++ PD.K8S.API.B/Controllers/ApiBController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace PD.K8S.API.B.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ApiBController : ControllerBase
+    {
+        // GET: api/<ApiBController>
+        [HttpGet]
+        public string Get()
+        {
+            return "Api B: Hello World!";
+        }
+
+        // GET api/<ApiBController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<ApiBController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<ApiBController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<ApiBController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
